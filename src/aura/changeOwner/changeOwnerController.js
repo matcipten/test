d22e37216d365aceb3303629faeeef2ba5be0288
Ten Group <ten.bc@tengroup.it>
2020-03-20 15:43:04 +0100

    SALESFORCE-690 Attivazione Servizio Omnichannel, SALESFORCE-821 Revisione UX Salesforce Mobile

---
+++ src/aura/changeOwner/changeOwnerController.js
@@ -0,0 +1,22 @@
+({
+	doInit : function(component, event, helper) {
+        helper.fetchUser(component);
+	},
+
+    onLogout : function(component, event, helper) {
+        console.log("Logout success.");
+        helper.changeOwner(component);
+        var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                "mode": 'pester',
+                "duration": 3000,
+                "title": "Attenzione",
+                "message": "I casi a te assegnati ritorneranno nella coda",
+                "type":'warning'
+            });
+			toastEvent.fire();
+		console.log("Vs user id: " +component.get("v.userId"));
+
+		
+    },
+})
